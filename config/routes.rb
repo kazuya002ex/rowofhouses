b8468dc5744b14eca82eff063243ae5b22f1f67a
harakazuya <hara@ex-corp.co.jp>
2019-12-03 17:19:53 +0900

    完了、未完了ごとの表示ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
+  get '/still', to: 'home#index'
+  get '/checked', to: 'home#index'
   namespace :api, { format: "json" } do
   	namespace :v1 do
   	  resources :todos, :only => [:index, :create, :update, :destroy]
