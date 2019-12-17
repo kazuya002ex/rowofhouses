fe4c49e8c8e5d38affc3b831e8590139f079b211
harakazuya <hara@ex-corp.co.jp>
2019-12-17 18:26:47 +0900

    本日の作業終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :todos, dependent: :destroy
+  # has_many :todos, dependent: :destroy
   validates :name, presence: true
   validates :password, presence: true
 end
