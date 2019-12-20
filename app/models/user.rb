0a699628a9eb24bb1c3ce2a7ccd2e43758bbabc4
harakazuya <hara@ex-corp.co.jp>
2019-12-20 18:36:59 +0900

    Modelに制約追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   # has_many :todos, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 20 }, format: { with: /\A[a-z0-9]+\z/i }
   validates :password, presence: true
 end
